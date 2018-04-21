1f5b3f6f2c4a152121ee022046fd8ad6fe8cccd3
Henrique <hdasller@gmail.com>
2018-04-21 04:29:36 -0300

    ajuste tema material ui

--- src/App.js
+++ src/App.js
@@ -4,21 +4,24 @@ import Nav from './Navbar';
 import ListVehicles from './ListVehicles';
 import Details from './Details';
 import DialogExampleModal from './Exemplo';
+import MuiThemeProvider from 'material-ui/styles/MuiThemeProvider'
 
 class App extends Component {
 
 
   render() {
     return (
+      <MuiThemeProvider>
       <div>
       <Nav></Nav>
       <ListVehicles></ListVehicles>
       <Details></Details>
       <DialogExampleModal></DialogExampleModal>
       </div>
+      </MuiThemeProvider>
+
     );
   }
 }
 
 export default App;
-  
